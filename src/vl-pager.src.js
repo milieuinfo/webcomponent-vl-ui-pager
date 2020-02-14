3e718d2bb14b8ddd96cf1fbdb50a6e25cc7ed2f2
coemans <tom.coemans@gmail.com>
2020-02-14 12:55:00 +0100

    fixed style path issues (#18)

--- src/vl-pager.src.js
+++ src/vl-pager.src.js
@@ -32,7 +32,7 @@ export class VlPager extends VlElement(HTMLElement) {
     super();
     this.shadow(`
       <style>
-        @import "../style.css";
+        @import '/style.css';
       </style>
       <div class="vl-pager">
         <ul id="pager-list" class="vl-pager__list">
