93178fa4cd34c13af69559157a852e7b50596308
Pascal Riquier <pascal.riquier@vlaanderen.be>
2020-06-09 12:10:26 +0200

    UIG-695 nieuwe versie util die nieuwe versie demo gebruikt om header en footer niet meer te renderen

--- test/e2e/pages/vl-pager.page.js
+++ test/e2e/pages/vl-pager.page.js
@@ -1,8 +1,6 @@
 const VlPager = require('../components/vl-pager');
 const {Page} = require('vl-ui-core').Test;
 const {Config} = require('vl-ui-core').Test;
-const {VlHeader} = require('vl-ui-header').Test;
-const {VlFooter} = require('vl-ui-footer').Test;
 const {VlElement} = require('vl-ui-core').Test;
 
 class VlPagerPage extends Page {
@@ -39,13 +37,7 @@ class VlPagerPage extends Page {
   }
 
   async load() {
-    await super.load(Config.baseUrl + '/demo/vl-pager.html');
-    //header en footer verwijderen, want anders verspringt de pagina eens die geladen wordt
-    //beter vervangen door een event om te checken wanneer die geladen is
-    const header = await new VlHeader(this.driver);
-    const footer = await new VlFooter(this.driver);
-    await header.remove();
-    await footer.remove();
+    await super.load(Config.baseUrl + '/demo/vl-pager.html?no-header=true&no-footer=true');
   }
 }
 
